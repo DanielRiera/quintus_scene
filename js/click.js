bcdcfead58ac5587f185fd0a2dcff4d7323e38c3
Daniel Riera <danielriera.p@gmail.com>
2016-11-09 23:51:35 +0100

    Add option "All"

--- js/click.js
+++ js/click.js
@@ -3,6 +3,7 @@ $(".generateScene").on("click", function(){
 	var height = $(".height_scene").val();
 	var number = $(".objects_tiles").val();
 	var widthCell = $(".width_cell").val();
+	urlTiles = $(".urlTiles").val();
 	console.log("Generating a scene ("+width+") for ("+height+")");
 	GS.generationScene(width, height, number,widthCell);
 });
@@ -18,6 +19,12 @@ $(".export").on("click", function(){
 	GS.exportBackground();
 	GS.exportCollision();
 });
-$(".changeTiles").on("click", function(){
-	urlTiles = $(".urlTiles").val();
+$(".all").on("click",function(){
+	var pos = objects[tilesActive];
+	$(".cel").css({
+		'width': '32px',
+		'background-image': 'url("'+urlTiles+'")',
+		'background-position': pos+'px 0',
+		'height': '32px'
+	});
 });
--- js/logic.js
+++ js/logic.js
@@ -31,7 +31,7 @@ var GS = {
 			var pos = '-'+parseInt(widthCell*i);
 			objects.push(pos);
 			var background = 'background: url("'+urlTiles+'") '+pos+'px 0;';
-			$(".objects").append("<li><div class='selectObject' data-i='"+i+"' style='width:"+widthCell+"px;height:"+widthCell+"px;"+background+"'></div><input type='number' data-i='"+i+"' class='collision' value='0' /></li>");
+			$(".objects").append("<li><div class='selectObject' data-i='"+i+"' style='width:"+widthCell+"px;height:"+widthCell+"px;"+background+"'></div><input type='number' data-i='"+i+"' class='collision' value='0' /><a class='all' data-i='"+i+"'>All</a></li>");
 		}
 		$(".objects").append("</ul>");
 		console.log(objects);
